ca7000f7e3cd3509a8d2098853797f044da5f382
Roja Patil <rojah@Rojas-MacBook-Air.local>
2023-11-05 21:32:01 -0700

    Added docstring

--- Solution.py
+++ Solution.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import sys
 
 #defining globals
 NUM_ROW_SEATS = 8
@@ -10,30 +11,52 @@ ROW_INDEX = 'A'
 SEATS_FILE = '/Users/rojah/airline_reservation.json'
 
 
-
 def load_seats():
+    """Returns seats information fetched from SEATS_FILE
+    """
     with open(SEATS_FILE, 'r') as f:
         return json.load(f)
 
+
 def store_seats(data):
+    """Stores seats information into SEATS_FILE
+
+    Args:
+        data (dict): Dictionary object containing seats information
+    """
     with open(SEATS_FILE, 'w') as f:
         json.dump(data, f, indent=4)
 
 
 def check_base_cases(row, seat_num, num_seats):
+    """Return 0 or 1 after checking if the input provided for booking/canceling a seat is valid.
+
+    Args:
+        row (int): This is the row number to be assigned a seat to. Row is originally a character, but is converted to an integer based on it's ascii value.
+        seat_num (int): The value of seat number between 0 and 7 to be booked or cancelled.
+        num_seats (int): The number of consecutive seats to be booked or cancelled.
+    """
     if (row < START_INDEX or row >= NUM_ROWS) or (seat_num <0 or seat_num >=NUM_ROW_SEATS) or (num_seats<1 or (seat_num + num_seats -1)>=NUM_ROW_SEATS):
         print('failing base case')
         print('FAIL')
         return 1
     return 0
 
+
 def book_seat(current_seats, row, seat_num, num_seats):
+    """Book the required seats if possible
+
+    Args:
+        current_seats (dict): Information on current seats in dictionary format.
+        row (int): This is the row number to be assigned a seat to. Row is originally a character, but is converted to an integer based on it's ascii value.
+        seat_num (int): The value of seat number between 0 and 7 to be booked or cancelled.
+        num_seats (int): The number of consecutive seats to be booked or cancelled.
+    """
     #check base cases
     ret = check_base_cases(row, seat_num, num_seats)
     if ret==1:
         return
     
-
     all_seats = current_seats[str(row)][seat_num:seat_num+num_seats]
     print(all_seats)
     #check if the seats are available
@@ -47,7 +70,16 @@ def book_seat(current_seats, row, seat_num, num_seats):
 
     print('SUCCESS')
 
+
 def cancel_seat(current_seats, row, seat_num, num_seats):
+    """Cancel required seats if possible
+
+    Args:
+        current_seats (dict): Information on current seats in dictionary format.
+        row (int): This is the row number to be assigned a seat to. Row is originally a character, but is converted to an integer based on it's ascii value.
+        seat_num (int): The value of seat number between 0 and 7 to be booked or cancelled.
+        num_seats (int): The number of consecutive seats to be booked or cancelled.
+    """
     ret = check_base_cases(row, seat_num, num_seats)
     if ret==1:
         return
@@ -75,7 +107,6 @@ if __name__=='__main__':
 
     #read the arguments
     args = parser.parse_args()
-    #print(args.ACTION)
 
     #if not already done, create initial layout for json file where seats assigned data is stored
     if not os.path.isfile(SEATS_FILE):
@@ -88,6 +119,9 @@ if __name__=='__main__':
     
     #call the required function based on the argument
     current_seats = load_seats()
+    if len(args.STARTING_SEAT_NUMBER)!=2:
+        print('FAIL')
+        sys.exit()
     row = ord(args.STARTING_SEAT_NUMBER[0])-ord(ROW_INDEX)
     seat_num = int(args.STARTING_SEAT_NUMBER[1:])
     num_seats = int(args.NUM_OF_SEATS)
